ab07ddfab5af30e8f091841f0bf300815c386ecf
Rizwan Qureshi <rizwan@ualberta.ca>
2019-12-28 23:33:29 -0700

    making a window

--- example/example.rb
+++ example/example.rb
@@ -1,37 +1,24 @@
 # frozen_string_literal: true
 
 require_relative '../crimson'
+require_relative '../lib/ruby/widgets/desktop'
+require_relative '../lib/ruby/widgets/window'
 
 
 server = Crimson::Server.new
 
-object = Crimson::Object.new("div")
-child = Crimson::Object.new("div")
+desktop = Crimson::Desktop.new
+desktop.style.backgroundColor = "white"
 
-child.style = {
-  "background-color": "green",
-  "height": "100px",
-  "width": "100px",
-}
+window = Crimson::Window.new("My Window")
+desktop.add(window)
 
-child.on("click") do |data|
-  puts data.to_s
-end
-
-object.style = {
-  "color": "white",
-  "background-color": "black",
-  "height": "100vh",
-  "width": "100vw"
-}
-object.innerHTML = "Hello World"
-object.add(child)
-object.commit_tree!
+desktop.commit_tree!
 
 server.on_connect do |client|
   puts "#{client.id} connected"
 
-  client.observe(object)
+  client.observe(desktop)
 end
 
 server.on_disconnect do |client|
